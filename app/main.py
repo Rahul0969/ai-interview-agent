ee8ab607bd5ae87cca59ed68a65c1752ab704d0b
Rahul9690 <rahuldubey0969@gmail.com>
2026-08-08 10:10:33 +0530

    Added interview session state

--- app/main.py
+++ app/main.py
@@ -9,17 +9,40 @@ class InterviewRequest(BaseModel):
     candidate : Optional[dict] = None
     message : Optional[str] = None
 
+#temporary storage for session
+sessions = {}
+
 @app.get("/")
 def root():
     return {"message": "AI Interview Agent is running"}
 
 @app.post("/api/interview")
 def interview(request : InterviewRequest):
+
+    session_id = request.sessionId
+
     if request.candidate is not None:
+
+        sessions[session_id] = {
+            "candidate": request.candidate,
+            "history" : []
+        }
         return {
             "reply": "Welcome. Let's bring your interview.",
             "done": False
         }
+    
+    if session_id not in sessions:
+        return {
+            "reply":"Session not found.Please start a new interview.",
+            "done": False
+        }
+
+    if request.message:
+        sessions[session_id]["history"].append({
+            "role":"candidate",
+            "message": request.message
+        })
     return {
         "reply": "Thank you for your answer.Let's continue further.",
         "done": False
