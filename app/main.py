d0c4bcea38bda73065ddcaa3051d95550874a94b
Rahul9690 <rahuldubey0969@gmail.com>
2026-08-08 09:27:54 +0530

    Added interview api endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI(title = "AI Interview Agent")
+
+class InterviewRequest(BaseModel):
+    sessionId : str
+    candidate : Optional[dict] = None
+    message : Optional[str] = None
 
 @app.get("/")
 def root():
     return {"message": "AI Interview Agent is running"}
+
+@app.post("/api/interview")
+def interview(request : InterviewRequest):
+    if request.candidate is not None:
+        return {
+            "reply": "Welcome. Let's bring your interview.",
+            "done": False
+        }
+    return {
+        "reply": "Thank you for your answer.Let's continue further.",
+        "done": False
+    }
